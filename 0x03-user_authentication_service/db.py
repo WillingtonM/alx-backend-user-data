ac934ce3f7bd36ee48321a61ae9c7973db958865
WillingtonM <willington.mhlanga@gmail.com>
2024-02-20 03:55:54 +0200

    0x03. User authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -15,20 +15,20 @@ class DB:
     """
 
     def __init__(self) -> None:
-        """ Initialize instance of new DB
+        """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session obj
+        """Memoized session object
         """
         if self.__session is None:
-            DBSess = sessionmaker(bind=self._engine)
-            self.__session = DBSess()
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
